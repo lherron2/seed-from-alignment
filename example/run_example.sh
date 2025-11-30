e0a7b93d9f0cf47dbff235c106d6bf165f49e519
Lukas Herron <lherron@umd.edu>
2025-11-30 12:28:13 -0500

    Added data from examples

--- example/run_example.sh
+++ example/run_example.sh
@@ -7,27 +7,27 @@ ROOT_DIR="${SCRIPT_DIR}/.."
 
 STO="${SCRIPT_DIR}/combined_1.cacofold.sto"
 COV="${SCRIPT_DIR}/combined_1.cacofold.cov"
-SEQ="6VMY_1_Chain"
-UNREFINED_DB="${SCRIPT_DIR}/6VMY_1_Chain_beta1.0_seed0.db"
-REFINED_DB="${SCRIPT_DIR}/6VMY_1_Chain_beta1.0_seed0_refined.db"
-ROSETTA_DB="${SCRIPT_DIR}/6VMY_1_rosetta.db"
-SUMMARY="${SCRIPT_DIR}/6VMY_summary.txt"
+NAME='9E5I'
+SEQ="${NAME}_1_Chains"
+UNREFINED_DB="${SCRIPT_DIR}/${SEQ}_beta1.0_seed0.db"
+REFINED_DB="${SCRIPT_DIR}/${SEQ}_Chain_beta1.0_seed0_refined.db"
+ROSETTA_DB="${SCRIPT_DIR}/${SEQ}_rosetta.db"
+SUMMARY="${SCRIPT_DIR}/${NAME}_summary.txt"
 
 
 echo "[INFO] Sampling structures for ${SEQ}"
 python "${ROOT_DIR}/sample_cli.py" "${STO}" \
   --seq "${SEQ}" \
-  --n-samples 100 \
+  --n-samples 1000 \
   --thin 1000 \
   --beta 1 \
   --cov-file "${COV}" \
   --cov-mode logE \
-  --cov-alpha 2 \
-  --cov-min-power 0 \
+  --cov-alpha 1 \
   --out-db "${UNREFINED_DB}" \
   --summary "${SUMMARY}" \
   --pk-filter-frac 0.2 \
-  --cov-forbid-negative
+  #--cov-forbid-negative
 
 python "${ROOT_DIR}/refine_unpaired_regions.py" \
   --sto "${STO}" \
